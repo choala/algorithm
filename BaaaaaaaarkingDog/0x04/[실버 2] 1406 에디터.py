3081b13c08b8d3177dfd4db3eb97a1976502f408
Chae-young Park <72439620+choala@users.noreply.github.com>
2024-02-06 22:50:26 +0900

    Update [실버 2] 1406 에디터.py
    출력 초과 > 수정 예정

--- BaaaaaaaarkingDog/0x04/[실버 2] 1406 에디터.py
+++ BaaaaaaaarkingDog/0x04/[실버 2] 1406 에디터.py
@@ -1,20 +1,20 @@
 import sys
 
-input = sys.stdin.readline().rstrip
-
 MAX = 600005
 data = [-1 for _ in range(MAX)]
 next = [-1 for _ in range(MAX)]
 prev = [-1 for _ in range(MAX)]
 
 unused = 1
+cursor = 0
+length = 0
 
 def traverse():
-    start = next[0]
+    target = next[0]
 
-    while start != -1:
-        print(chr(data[start]), end="")
-        start = next[start]
+    while target != -1:
+        print(chr(data[target]), end="")
+        target = next[target]
 
 def insert(pos, char):
     global unused
@@ -22,29 +22,40 @@ def insert(pos, char):
     data[unused] = ord(char)
     next[unused] = next[pos]
     prev[unused] = pos
-    
+
+    next[pos] = unused
+    if next[unused] != -1:
+        prev[next[unused]] = unused
+
     unused += 1
 
 def delete(pos):
     next[prev[pos]] = next[pos]
-    prev[next[pos]] = prev[pos]
+    if next[pos] != -1:
+        prev[next[pos]] = prev[pos]
 
 text = input()
-cursor = len(text)   
+length = len(text)
 for t in text:
-    insert(unused, t)
+    insert(cursor, t)
+    cursor += 1
 
 m = int(input())
 for _ in range(m):
-    command = input().split()
-
+    command = sys.stdin.readline().rstrip().split()
+    
     if len(command) == 1:
-        if command[0] == "L":
+        if command[0] == "L" and cursor != 0:
             cursor -= 1
-        elif command[0] == "D":
+        elif command[0] == "D" and cursor + 1 <= length:
             cursor += 1
-        else:
+        elif command[0] == "B" and cursor != 0:
             delete(cursor)
-            
+            cursor -= 1
+            length -= 1
     else:
-        insert(cursor, "$")
+        insert(cursor, command[1])
+        cursor += 1
+        length += 1
+
+traverse()

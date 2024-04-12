651e8be8a0f17a536f5f4df82cad8a90079ed477
Chae-young Park <72439620+choala@users.noreply.github.com>
2024-04-13 00:03:23 +0900

    Create [Level 1] 폰켓몬.py

---
+++ programmers/해시/[Level 1] 폰켓몬.py
@@ -0,0 +1,13 @@
+import itertools
+
+def solution(nums):
+    selected = []
+    
+    index = 0
+    while len(selected) < len(nums) // 2 and index < len(nums):
+        if nums[index] not in selected:
+            selected.append(nums[index])
+            
+        index += 1
+        
+    return len(selected)

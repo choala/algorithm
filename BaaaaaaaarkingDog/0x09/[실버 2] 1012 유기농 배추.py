aa1a754f442ec09bfeb5d57bce27c70eea8864bc
Chae-young Park <72439620+choala@users.noreply.github.com>
2025-05-22 16:39:40 +0900

    Update [실버 2] 1012 유기농 배추.py
    visit 배열 없이 처리하는 방식으로 변경

--- BaaaaaaaarkingDog/0x09/[실버 2] 1012 유기농 배추.py
+++ BaaaaaaaarkingDog/0x09/[실버 2] 1012 유기농 배추.py
@@ -5,43 +5,40 @@ input = sys.stdin.readline
 
 t = int(input())
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+queue = deque()
+
 for _ in range(t):
     m, n, k = map(int, input().split())
+    count = 0
 
-    # BFS 기본 세팅
     cabbage = [[0 for _ in range(m)] for _ in range(n)]
-    visit = [[0 for _ in range(m)] for _ in range(n)]
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
 
-    queue = deque()
-
-    # cabbage 기본 세팅
+    # 배추 밭 세팅
     for _ in range(k):
-        x, y = map(int, input().split())
+        y, x = map(int, input().split())
+        cabbage[x][y] = 1
 
-        cabbage[y][x] = 1
-        
-    white = 0
-    
     for i in range(n):
         for j in range(m):
-            # 시작점 찾기
-            if cabbage[i][j] == 1 and visit[i][j] == 0:
-                visit[i][j] = 1
+            if cabbage[i][j] == 1:
+                cabbage[i][j] = 0
                 queue.append((i, j))
-                white += 1
-    
-            while queue:
-                x, y = queue.popleft()
-    
-                for direc in range(4):
-                    nx = x + dx[direc]
-                    ny = y + dy[direc]
-    
-                    if nx >= 0 and nx < n and ny >= 0 and ny < m:
-                        if cabbage[nx][ny] == 1 and visit[nx][ny] == 0:
-                            visit[nx][ny] = 1
-                            queue.append((nx, ny))
-    
-    print(white)
+
+                count += 1
+
+                while queue:
+                    x, y = queue.popleft()
+                    
+                    for dir in range(4):
+                        nx = x + dx[dir]
+                        ny = y + dy[dir]
+
+                        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+                            if cabbage[nx][ny] == 1:
+                                cabbage[nx][ny] = 0
+                                queue.append((nx, ny))
+                                
+    print(count)

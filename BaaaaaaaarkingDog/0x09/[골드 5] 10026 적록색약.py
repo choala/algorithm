599f30183c115c3fba8f10f347c51b308d1a7983
Chae-young Park <72439620+choala@users.noreply.github.com>
2025-05-22 17:12:15 +0900

    Update [골드 5] 10026 적록색약.py
    기존) 삼중 for 문을 돌며 'R', 'G', 'B' 각 색상에 해당하는 구역을 모두 처리하고 넘어가는 방식
    
    변경) 이중 for 문으로 그림을 탐색하며 color 변수에 구역의 기준 색상을 할당하는 방식

--- BaaaaaaaarkingDog/0x09/[골드 5] 10026 적록색약.py
+++ BaaaaaaaarkingDog/0x09/[골드 5] 10026 적록색약.py
@@ -5,69 +5,77 @@ input = sys.stdin.readline
 
 n = int(input())
 
+# BFS 세팅
+redGreenBlue = [[False for _ in range(n)] for _ in range(n)]
+redGreen = [[False for _ in range(n)] for _ in range(n)]
 
-# paint 기본 세팅
-paint = []
-for _ in range(n):
-    row = list(input().strip())
-    paint.append(row)
-    
-# BFS 기본 세팅
-visit = [[0 for _ in range(len(paint[0]))] for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 queue = deque()
 
-rgb_count = 0
-rb_count = 0
+# 그림 세팅
+paint = []
 
-# 적록색약이 아닌 경우의 BFS
-rgb = ['R', 'G', 'B']
-for index in range(3):
-    for i in range(n):
-        for j in range(len(paint[0])):
-            if paint[i][j] == rgb[index] and visit[i][j] == 0:
-                visit[i][j] = 1
-                queue.append((i, j))
-                
-                rgb_count += 1
-    
-                while queue:
-                    x, y = queue.popleft()
-                    
-                    for direc in range(4):
-                        nx = x + dx[direc]
-                        ny = y + dy[direc]
-    
-                        if nx >= 0 and nx < n and ny >= 0 and ny < len(paint[0]):
-                            if paint[nx][ny] == rgb[index] and visit[nx][ny] == 0:
-                                visit[nx][ny] = 1
+for _ in range(n):
+    row = list(input().rstrip())
+    paint.append(row)
+
+# 적록색약이 아닌 사람이 보는 구역의 수 찾기
+redGreenBlueCount = 0
+
+for i in range(n):
+    for j in range(n):
+        if not redGreenBlue[i][j]:
+            redGreenBlue[i][j] = True
+            queue.append((i, j))
+
+            # 기준 색상 정하기
+            color = paint[i][j]
+            redGreenBlueCount += 1
+
+            while queue:
+                x, y = queue.popleft()
+
+                for dir in range(4):
+                    nx = x + dx[dir]
+                    ny = y + dy[dir]
+
+                    if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                        if paint[nx][ny] == color and not redGreenBlue[nx][ny]:
+                            redGreenBlue[nx][ny] = True
+                            queue.append((nx, ny))
+
+# 적록색약안 사람이 보는 구역의 수 찾기
+redGreenCount = 0
+
+for i in range(n):
+    for j in range(n):
+        if not redGreen[i][j]:
+            redGreen[i][j] = True
+            queue.append((i, j))
+
+            # 기준 색상 정하기
+            color = paint[i][j]
+            redGreenCount += 1
+
+            while queue:
+                x, y = queue.popleft()
+
+                for dir in range(4):
+                    nx = x + dx[dir]
+                    ny = y + dy[dir]
+
+                    if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                        # 파랑을 기준으로 구역을 찾을 때
+                        if color == 'B':
+                            if paint[nx][ny] == color and not redGreen[nx][ny]:
+                                redGreen[nx][ny] = True
+                                queue.append((nx, ny))
+                        else:
+                            if (paint[nx][ny] == 'R' or paint[nx][ny] == 'G') and not redGreen[nx][ny]:
+                                redGreen[nx][ny] = True
                                 queue.append((nx, ny))
 
-# 적록색약인 경우의 BFS
-visit = [[0 for _ in range(len(paint[0]))] for _ in range(n)]
-
-rgb = ['RG', 'B']
-for index in range(2):
-    for i in range(n):
-        for j in range(len(paint[0])):
-            if paint[i][j] in rgb[index] and visit[i][j] == 0:
-                visit[i][j] = 1
-                queue.append((i, j))
-
-                rb_count += 1
-
-                while queue:
-                    x, y = queue.popleft()
-
-                    for direc in range(4):
-                        nx = x + dx[direc]
-                        ny = y + dy[direc]
-
-                        if nx >= 0 and nx < n and ny >= 0 and ny < len(paint[0]):
-                            if paint[nx][ny] in rgb[index] and visit[nx][ny] == 0:
-                                visit[nx][ny] = 1
-                                queue.append((nx, ny))
-
-print(rgb_count, rb_count)
+print(redGreenBlueCount)
+print(redGreenCount)
